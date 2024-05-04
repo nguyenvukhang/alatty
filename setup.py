3fe765018d3fe775e23cf4a8d33c223c9824ac59
nguyenvukhang <brew4k@gmail.com>
2024-05-04 08:13:29 +0800

    Drop: ServiceProvider

--- setup.py
+++ setup.py
@@ -1156,21 +1156,6 @@ def macos_info_plist() -> bytes:
         },
     ]
 
-    services = [
-        {
-            'NSMenuItem': {'default': f'New {appname} Tab Here'},
-            'NSMessage': 'openTab',
-            'NSRequiredContext': {'NSTextContent': 'FilePath'},
-            'NSSendTypes': ['NSFilenamesPboardType', 'public.plain-text'],
-        },
-        {
-            'NSMenuItem': {'default': f'New {appname} Window Here'},
-            'NSMessage': 'openOSWindow',
-            'NSRequiredContext': {'NSTextContent': 'FilePath'},
-            'NSSendTypes': ['NSFilenamesPboardType', 'public.plain-text'],
-        },
-    ]
-
     pl = dict(
         # Naming
         CFBundleName=appname,
@@ -1208,7 +1193,7 @@ def macos_info_plist() -> bytes:
         # Document and URL Types
         CFBundleDocumentTypes=docs,
         # Services
-        NSServices=services,
+        NSServices=[],
         # Calendar and Reminders
         NSCalendarsUsageDescription=access('your calendar data.'),
         NSRemindersUsageDescription=access('your reminders.'),
