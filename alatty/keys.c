02b4f0ad29ec617b3d37e30c894bd02fd67bb416
nguyenvukhang <brew4k@gmail.com>
2025-03-20 09:42:09 +0800

    x: HANDLE_TERMIOS_SIGNALS

--- alatty/keys.c
+++ alatty/keys.c
@@ -229,9 +229,6 @@ on_key_input(GLFWkeyevent *ev) {
         schedule_write_to_child(w->id, 1, text, strlen(text));
         debug("sent key as text to child: %s\n", text);
     } else if (size > 0) {
-        if (size == 1 && screen->modes.mHANDLE_TERMIOS_SIGNALS) {
-            if (screen_send_signal_for_key(screen, *encoded_key)) return;
-        }
         schedule_write_to_child(w->id, 1, encoded_key, size);
         if (OPT(debug_keyboard)) {
             debug("sent encoded key to child: ");
