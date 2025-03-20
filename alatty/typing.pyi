f279771a78f689a0949fb9c42ee590ee74f3def3
nguyenvukhang <brew4k@gmail.com>
2025-03-20 10:24:14 +0800

    x: kittens.tui (alatty/typing.pyi)

--- alatty/typing.pyi
+++ alatty/typing.pyi
@@ -7,15 +7,6 @@ from typing import Literal
 from typing import Protocol as Protocol
 from typing import TypedDict as TypedDict
 
-from kittens.tui.handler import Handler as HandlerType
-from kittens.tui.images import GraphicsCommand as GraphicsCommandType
-from kittens.tui.images import ImageManager as ImageManagerType
-from kittens.tui.loop import Debug as Debug
-from kittens.tui.loop import Loop as LoopType
-from kittens.tui.loop import MouseButton as MouseButton
-from kittens.tui.loop import MouseEvent as MouseEvent
-from kittens.tui.loop import TermManager as TermManagerType
-
 from .boss import Boss as BossType
 from .child import Child as ChildType
 from .conf.utils import BadLine as BadLineType
@@ -58,10 +49,10 @@ class WindowSystemMouseEvent(TypedDict):
 
 __all__ = (
     'EdgeLiteral', 'MatchType', 'GRT_a', 'GRT_f', 'GRT_t', 'GRT_o', 'GRT_m', 'GRT_d',
-    'GraphicsCommandType', 'HandlerType', 'AbstractEventLoop', 'AddressFamily', 'Socket', 'CompletedProcess',
-    'PopenType', 'Protocol', 'TypedDict', 'ImageManagerType', 'Debug', 'LoopType', 'MouseEvent',
-    'TermManagerType', 'BossType', 'ChildType', 'BadLineType', 'MouseButton',
-    'KeyActionType', 'KeyMap', 'AlattyCommonOpts', 'AliasMap', 'CoreTextFont', 'WindowSystemMouseEvent',
-    'FontConfigPattern', 'ScreenType', 'StartupCtx', 'KeyEventType', 'LayoutType', 'PowerlineStyle',
-    'RemoteCommandType', 'SessionType', 'SessionTab', 'SpecialWindowInstance', 'TabType', 'ScreenSize', 'WindowType'
+    'AbstractEventLoop', 'AddressFamily', 'Socket', 'CompletedProcess', 'PopenType',
+    'Protocol', 'TypedDict', 'BossType', 'ChildType', 'BadLineType', 'KeyActionType',
+    'KeyMap', 'AlattyCommonOpts', 'AliasMap', 'CoreTextFont', 'WindowSystemMouseEvent',
+    'FontConfigPattern', 'ScreenType', 'StartupCtx', 'KeyEventType', 'LayoutType',
+    'PowerlineStyle', 'RemoteCommandType', 'SessionType', 'SessionTab',
+    'SpecialWindowInstance', 'TabType', 'ScreenSize', 'WindowType'
 )
