978431d180e5f891f52bd9e91e1a1ac9749970e5
nguyenvukhang <brew4k@gmail.com>
2025-04-06 11:29:17 +0800

    x: GenerateCompletions

--- tools/cli/completion-main.go
+++ tools/cli/completion-main.go
@@ -4,10 +4,6 @@ package cli
 
 import (
 	"encoding/json"
-	"fmt"
-	"io"
-	"os"
-	"strings"
 )
 
 func json_input_parser(data []byte, shell_state map[string]string) ([][]string, error) {
@@ -35,72 +31,3 @@ func init() {
 }
 
 var registered_exes []func(root *Command)
-
-func GenerateCompletions(args []string) error {
-	output_type := "json"
-	if len(args) > 0 {
-		output_type = args[0]
-		args = args[1:]
-	}
-	n := len(args)
-	if n < 1 {
-		n = 1
-	}
-	if output_type == "setup" {
-		if len(args) == 0 {
-			return fmt.Errorf("The shell must be specified")
-		}
-		shell_name := args[0]
-		args = args[1:]
-		completion_script := completion_scripts[shell_name]
-		if completion_script == nil {
-			return fmt.Errorf("Unsupported shell: %s", shell_name)
-		}
-		output, err := completion_script(args)
-		if err == nil {
-			_, err = os.Stdout.WriteString(output)
-		}
-		return err
-	}
-	shell_state := make(map[string]string, n)
-	for _, arg := range args {
-		k, v, found := strings.Cut(arg, "=")
-		if !found {
-			return fmt.Errorf("Invalid shell state specification: %s", arg)
-		}
-		shell_state[k] = v
-	}
-	input_parser := input_parsers[output_type]
-	output_serializer := output_serializers[output_type]
-	if input_parser == nil || output_serializer == nil {
-		return fmt.Errorf("Unknown output type: %s", output_type)
-	}
-	data, err := io.ReadAll(os.Stdin)
-	if err != nil {
-		return err
-	}
-	// debugf("%#v", string(data))
-	all_argv, err := input_parser(data, shell_state)
-	if err != nil {
-		return err
-	}
-	var root = NewRootCommand()
-	for _, re := range registered_exes {
-		re(root)
-	}
-
-	err = root.Validate()
-	if err != nil {
-		return err
-	}
-	all_completions := make([]*Completions, 0, 1)
-	for _, argv := range all_argv {
-		all_completions = append(all_completions, root.GetCompletions(argv, init_completions[output_type]))
-		root.ResetAfterParseArgs()
-	}
-	output, err := output_serializer(all_completions, shell_state)
-	if err == nil {
-		_, err = os.Stdout.Write(output)
-	}
-	return err
-}
