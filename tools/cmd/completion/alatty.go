fc70dfccf050565895dce7a525aa4c4ed68f8437
nguyenvukhang <brew4k@gmail.com>
2025-04-06 11:33:46 +0800

    ∂: RunFunc

--- tools/cmd/completion/alatty.go
+++ tools/cmd/completion/alatty.go
@@ -17,7 +17,7 @@ func EntryPoint(tool_root *cli.Command) {
 		ShortDescription: "Generate completions for alatty commands",
 		HelpText:         "Generate completion candidates for alatty commands. The command line is read from STDIN. output_type can be one of the supported shells: :code:`zsh`, :code:`fish`, :code:`bash`, or :code:`setup` for completion setup script following with the shell name, or :code:`json` for JSON output.",
 		Run: func(cmd *cli.Command, args []string) (ret int, err error) {
-			return ret, cli.GenerateCompletions(args)
+			return ret, nil
 		},
 	})
 }
