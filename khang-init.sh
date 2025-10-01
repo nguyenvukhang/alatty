2fb1aa9f6e9ec3976f79e1317514eb78036c0239
nguyenvukhang <brew4k@gmail.com>
2025-10-01 14:33:06 +0800

    Add a setup script

---
+++ khang-init.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+rm -rf .editorconfig
+rm -rf Brewfile
+rm -rf CHANGELOG.rst
+rm -rf CONTRIBUTING.md
+rm -rf INSTALL.md
+rm -rf Makefile
+rm -rf README.asciidoc
+rm -rf SECURITY.md
+rm -rf count-lines-of-code
+rm -rf dev.sh
+rm -rf key_encoding.json
+rm -rf publish.py
+rm -rf session.vim
+rm -rf shell.nix
+rm -rf staticcheck.conf
+rm -rf rsync-and-build.sh
+rm -rf update-on-ox
